bbe3ae4f04bf63053648ca9e93ecad9505fe4f61
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-06-09 10:03:56 -0700

    sets up some seed data for animal sightings

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,10 +7,34 @@ import { Component } from '@angular/core';
     <div class="jumbotron">
       <h1>Orlando Wetlands Park</h1>
     </div>
+    <h2>Animal Sightings</h2>
+    <ul>
+      <li *ngFor="let currentAnimal of animals">{{currentAnimal.species}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  animals: Animal[] = [
+    new Animal('Hooded Merganser', 'adult', 'fish', 'Lake Searcy', 'male', 'mollusks', 'garbage', '6/8/17', 'uncommon', 'picgoeshere'),
+    new Animal('Annihinga', 'adult', 'young alligators', 'Cypress Swamp', 'female', 'juicy leeches', 'getting alligators stuck in throat', '6/8/17', 'common', 'picgoeshere'),
+    new Animal('Eastern Towhee', 'adult', 'mosquitos, wild blackberries', 'Limpkin Lane', 'female', 'succulent snails', 'noisy gallinules', '6/8/17', 'common', 'picgoeshere'),
+    new Animal('Ring-billed Gull', 'juvenile', 'rodents, eggs', 'Alligator Alley', 'male', 'preening', 'tourists', '4/1/17', 'rare', 'img')
+  ]
+}
 
+export class Animal {
+  constructor(
+    public species: string,
+    public age: string,
+    public diet: string,
+    public location: string,
+    public sex: string,
+    public likes: string,
+    public dislikes: string,
+    public lastSeen: string,
+    public commonality: string,
+    public picture: string
+  ){}
 }
